439739e65ba1dba4c0c5c9948fef8f6807ecb0da
João <soyjog@gmail.com>
2020-07-17 12:36:09 -0300

    Criei versão inicial para blur multithread

---
+++ src/blur_threads.c
@@ -0,0 +1,66 @@
+#include "../images/imageprocessing.h"
+#include "blur.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+
+#define N 5
+
+// Struct para guardar apenas 1 cor e a largura e altura dela
+typedef struct argument{
+	float *color;
+	int width, height;
+} argument;
+
+void *worker(void *arg){
+	argument *real_arg = (argument *) arg; 				// Faz um cast para o tipo original
+	blur(real_arg->color, real_arg->height, real_arg->width, N); 	// Realiza o blur
+	
+	return (void *) real_arg->color; 				// Retorna o novo vetor com o blur aplicado
+}
+
+
+int main(){
+	pthread_t r, g, b;
+	void *new_r, *new_g, *new_b;
+
+	// Abre a imagem
+	image I = abrir_imagem("../images/data/cachorro.jpg");
+
+	// Cria as srtucts de argumento para cada thread
+	argument *r_arg, *g_arg, *b_arg;
+	
+	r_arg->color = I.r;
+	r_arg->width = I.width;
+	r_arg->height = I.height;
+	
+	g_arg->color = I.g;
+	g_arg->width = I.width;
+	g_arg->height = I.height;
+
+	b_arg->color = I.b;
+	b_arg->width = I.width;
+	b_arg->height = I.height;
+	
+	// Cria uma thread para tratar de cada cor independentemente
+	pthread_create(&r, NULL, worker, (void *) r_arg);
+	pthread_create(&g, NULL, worker, (void *) g_arg);
+	pthread_create(&b, NULL, worker, (void *) b_arg);
+
+	// Salva os retornos de cada thread
+	pthread_join(r, &new_r);
+	pthread_join(g, &new_g);
+	pthread_join(b, &new_b);
+
+	// Atualiza a imagem com os novos valores com blur
+	I.r = (float *) new_r;
+	I.g = (float *) new_g;
+	I.b = (float *) new_b;
+	
+	// Salva a imagem e libera ela
+	salvar_imagem("../Results/thread.jpg", &I);
+	liberar_imagem(&I);
+	
+	return 0;
+}
