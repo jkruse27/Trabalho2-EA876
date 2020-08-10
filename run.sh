ecdfb25f10ab9551367eadcf558395222ca52ea2
João <soyjog@gmail.com>
2020-08-10 19:06:16 -0300

    Substituimos time por cronometro

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ for img in $IMGS
 do
 	echo $img $i
 	new=${img/data\//}
-#	./$LIN $img saída/${new/.jpg/_linear.jpg} > /dev/null
-#	./$THR $img saída/${new/.jpg/_thread.jpg} > /dev/null
+	./$LIN $img saída/${new/.jpg/_linear.jpg} > /dev/null
+	./$THR $img saída/${new/.jpg/_thread.jpg} > /dev/null
 	./$PRO $img saída/${new/.jpg/_process.jpg}> /dev/null
 done
